0ca57be00eb90517484ed9467b3abe557db32c95
musekwa <musekwa2011@gmail.com>
2021-06-18 10:25:02 +0200

    Update MongoDB url and app port

--- config/Config.js
+++ config/Config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  DB: 'mongodb://localhost/movie_rating_app',
+  DB: 'mongodb+srv://eva:$Ssssssss12@cluster0.04c52.mongodb.net/movie_rating_app?retryWrites=true&w=majority',
   SECRET: 'movieratingappsecretkey',
 }
